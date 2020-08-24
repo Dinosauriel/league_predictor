f3ce3225709ee971835975da61436774d2487da7
Aurel Feer <aurel.feer@gmail.com>
2020-08-25 00:43:19 +0200

    initial commit

---
+++ config.example.py
@@ -0,0 +1,6 @@
+#your api key obtained from the riot api
+api_key = "AAAAA-AAAAAA-AAAAAA"
+#server region
+region = "EUW1"
+#summoner name for the match search root (to obtain training data)
+root_summoner_name = "summoner name"
---
+++ scrape_games.py
@@ -0,0 +1,4 @@
+import numpy as np
+import config as cfg
+
+print(cfg.api_key)
