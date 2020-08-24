8544a2abae6fa6602579093213fdacb271bf2f37
Aurel Feer <aurel.feer@gmail.com>
2020-08-25 01:42:05 +0200

    implemented match history id fetching

--- riot_api.py
+++ riot_api.py
@@ -6,10 +6,13 @@ HOST = "https://" + cfg.host
 HEADERS = {"X-Riot-Token": cfg.api_key}
 
 #path: resource without hostname
-def get(path: str):
-	r = requests.get(HOST + path, headers=HEADERS)
+def get(path: str, parameters: dict={}):
+	r = requests.get(HOST + path, headers=HEADERS, params=parameters)
 	return json.loads(r.text)
 
 
 def getAccountId(summoner_name: str):
 	return get("/lol/summoner/v4/summoners/by-name/" + summoner_name)["accountId"]
+
+def getMatchHistory(account_id: str):
+	return get("/lol/match/v4/matchlists/by-account/" + account_id)["matches"]
--- scrape_games.py
+++ scrape_games.py
@@ -2,4 +2,14 @@ import riot_api as api
 import config as cfg
 
 
-print(api.getAccountId(cfg.root_summoner_name))
+account_id = api.getAccountId(cfg.root_summoner_name)
+print("fetched account id: " + account_id)
+
+match_history = api.getMatchHistory(account_id)
+print("fetched " + str(len(match_history)) + " most recent matches for account id " + account_id)
+
+match_history_ids = []
+for match in match_history:
+	match_history_ids.append(match['gameId'])
+
+print(match_history_ids)
